dbca32571a850078e2538576a65d3c4b10d2fbd4
Justin Lane <justin.l.lane@gmail.com>
2016-09-04 13:59:34 -0600

    20160904 1358 - Got nested ng-repeats working.
    Need to remember the <p> cannot have block elements in them.

--- be2.ang.js
+++ be2.ang.js
@@ -1,75 +1,79 @@
 (function(){
 	var app = angular.module("be2",[]);
 
-	app.controller("be2MainController",["$scope",function($scope){
+	// FleetService - This service provides fleet functions and storage.
+	app.factory("FleetService",function() {
+		var fleets = [];
+
+		return {
+			all: function() { return fleets;},
+			add: function(f) { fleets.push(f); },
+			infoAll: function() {
+				var info = [];
+				for(var f in fleets) {
+					info.push(fleets[f].name);
+				}
+				return info;
+			}
+		}
+	});
+
+	app.controller("be2MainController",["$scope","FleetService",function($scope,FleetService){
 		$scope.states = {
 			combat: "combat",
 			fleets: "fleets",
 			units: "units"
 		};
-		$scope.state = $scope.states.units;
-		$scope.udl = "{name: \"\"}";
+		$scope.state = $scope.states.combat;
 
-		var units = [];
-		$scope.units = units;
-		// Some example units
-		units.push({
-			"name": "GTVA Wasp",
-			"type": "fighter",
-			"beam": 1,
-			"missile": 4,
-			"shield": 0,
-			"hull": 1
+		FleetService.add({
+			"name": "The Heavy",
+			"units": [{
+				"unit": {
+					"name": "Emma Rose",
+					"type": "starship",
+					"defense": 15
+				},
+				"hull": {
+					"base": 3,
+					"max": 5
+				},
+				"shield": {
+					"max": 1
+				},
+				"direct-fire": [{
+					"volley": 4,
+					"target": 15
+				}]
+			}]
 		});
 
-		var fleets = {};
-		$scope.fleets = fleets;
-		
-		var theHeavy = {};
-		theHeavy.name = "The Heavy";
-		theHeavy.units = [{
-			"unit": {
-				"name": "Emma Rose",
-				"type": "starship",
-				"defense": 15
-			},
-			"hull": {
-				"base": 3,
-				"max": 5
-			},
-			"shield": {
-				"max": 1
-			},
-			"direct-fire": [{
-				"volley": 4,
-				"target": 15
+		FleetService.add({
+			"name": "The Scourge",
+			"units": [{
+				"unit": {
+					"name": "Scourge A",
+					"type": "starship",
+					"defense": 15
+				},
+				"hull": {
+					"base": 1,
+					"max": 3
+				},
+				"direct-fire": [{
+					"volley": 3,
+					"target": 15
+				}],
+				"fire-packet": [{
+					"packet": 2,
+					"size": 1,
+					"ammo": 1
+				}]
 			}]
-		}];
+		});
 
-		var theScourge = {};
-		theScourge.name = "The Scourge";
-		theScourge["Scourge A"] = {
-			"unit": {
-				"type": "starship",
-				"defense": 15
-			},
-			"hull": {
-				"base": 1,
-				"max": 3
-			},
-			"direct-fire": [{
-				"volley": 3,
-				"target": 15
-			}],
-			"fire-packet": [{
-				"packet": 2,
-				"size": 1,
-				"ammo": 1
-			}]
-		};
 
-		$scope.fleets["The Heavy"] = theHeavy;
-		//$scope.fleets["The Scourge"] = theScourge;
+		$scope.fleets = FleetService.all();
 	}]);
 
 	app.controller("be2CombatController",["$scope",function($scope){

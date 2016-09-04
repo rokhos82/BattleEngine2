57f3cc5e38a71051ae9843e2b24e9f1d812c0eee
Justin Lane <justin.l.lane@gmail.com>
2016-09-03 23:33:23 -0600

    20160903 2332 - Combat Engine First Attempt
    Trying to get some basic functionality of the combat engine working.
    The largest first step is the UDL for the units in the fleet.

--- be2.ang.js
+++ be2.ang.js
@@ -21,6 +21,58 @@
 			"shield": 0,
 			"hull": 1
 		});
+
+		var fleets = {};
+		$scope.fleets = fleets;
+		
+		var theHeavy = {};
+		theHeavy.name = "The Heavy";
+		theHeavy.units = [{
+			"unit": {
+				"name": "Emma Rose",
+				"type": "starship",
+				"defense": 15
+			},
+			"hull": {
+				"base": 3,
+				"max": 5
+			},
+			"shield": {
+				"max": 1
+			},
+			"direct-fire": [{
+				"volley": 4,
+				"target": 15
+			}]
+		}];
+
+		var theScourge = {};
+		theScourge.name = "The Scourge";
+		theScourge["Scourge A"] = {
+			"unit": {
+				"type": "starship",
+				"defense": 15
+			},
+			"hull": {
+				"base": 1,
+				"max": 3
+			},
+			"direct-fire": [{
+				"volley": 3,
+				"target": 15
+			}],
+			"fire-packet": [{
+				"packet": 2,
+				"size": 1,
+				"ammo": 1
+			}]
+		};
+
+		$scope.fleets["The Heavy"] = theHeavy;
+		//$scope.fleets["The Scourge"] = theScourge;
+	}]);
+
+	app.controller("be2CombatController",["$scope",function($scope){
 	}]);
 
 	app.directive('unitPanel',function() {
@@ -34,6 +86,13 @@
 		return {
 			restrict: 'E',
 			templateUrl: 'templates/fleet-panel.html'
+		};
+	});
+
+	app.directive('combatPanel',function() {
+		return {
+			restrict: 'E',
+			templateUrl: 'templates/combat-panel.html'
 		};
 	});
 })();

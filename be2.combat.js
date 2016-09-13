476ca9accbc6acd02ec92f8b6d7291ca9e482de0
Justin Lane <justin.l.lane@gmail.com>
2016-09-13 16:03:58 -0600

    20160913 1602 - Some work on the combat rewrite.
    Going to have the web worker only do one round of combat at a time.
    Let the main app use a setTimeout throttle to keep from overwhelming the
    UI.  This way I can use better libraries to keep track of round state
    information and logs.

--- be2.combat.js
+++ be2.combat.js
@@ -1,9 +1,7 @@
 (function(){
 	var _log = "";
-	var _done = false;
 	var state = undefined;
-	var angular = undefined;
-
+	
 	function sleep(milliseconds) {
 		var date = new Date();
 		var curDate = null;
@@ -17,21 +15,48 @@
 		return Math.floor((Math.random() * (high - low)) + low);
 	}
 
-	function logger(entry) {
-		_log = entry + "\n";
-		syncLog();
+	function chooseCombatant(faction) {
+		var c = undefined;
+		
+		// Get a list of enemies for the faction
+		var enemies = state.combatant[faction].enemies;
+		if(enemies.length > 0) {
+			// Select a random enemy faction to attack
+			var k = randomBetween(0,enemies.length - 1);
+			c = enemies[k];
+		}
+		else {
+			break;
+		}
+		
+		return c;
+	};
+
+	function selectTarget() {
+		for(var c in state.combatants) {
+			var faction = state.combatants[c];
+			var fleets = faction.fleets;
+
+			// Which enemy faction do we pick from?
+			var targetFaction = chooseCombatant(faction.name);
+
+			// Get a list of targets from the faction
+			var targets = state.combatants[targetFaction].units;
+		}
 	}
 
-	function syncLog() {
-		this.postMessage({log:_log,done:_done});
-	}
-
-	function initCombat(event) {
-		// Save the state sent to the worker thread
+	function doRound(event) {
 		state = event.data.state;
 
-		// Get the reference to angular sent to the worker thread
+		// Select targets
+
+		// Resolve hits
+
+		// Change values in state depending on targets & hits
+
+		// Transmit the state back to the main application
+		this.postMessage(state);
 	}
 
-	this.addEventListener('message',initCombat,false);
+	this.addEventListener('message',doRound,false);
 })();
